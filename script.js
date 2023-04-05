52ffe913e1d4d29c7c112a4fdaffab9c44ee756e
Keil-ENZO <enzo.keil06@icloud.com>
2023-04-05 22:15:39 +0200

    probleme magicarpe

--- script.js
+++ script.js
@@ -1,277 +1,373 @@
-const canvas = document.getElementById('game-canvas');
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
 let platFormGap = 0;
 let score = 0;
 let highScore = 0;
 
 class Doodler {
-    constructor() {
-        this.context = canvas.getContext("2d");
-        this.x = canvas.width / 2;
-        this.y = canvas.height - 200;
-        this.image = new Image();
-        this.image.src = './assets/personnage.png';
-        this.prevY = this.y;
-        this.width = 50;
-        this.height = 50;
-        this.vx = 0;
-        this.vy = 0;
-        this.gravity = 0.03;
-        this.jumpStrength = -2.5;
+  constructor() {
+    this.context = canvas.getContext("2d");
+    this.x = canvas.width / 4;
+    this.y = canvas.height - 600;
+    this.image = new Image();
+    this.image.src = "./assets/personnage.png";
+    this.prevY = this.y;
+    this.width = 80;
+    this.height = 80;
+    this.vx = 0;
+    this.vy = 0;
+    this.gravity = 0.06;
+    this.jumpStrength = -4;
+  }
+
+  // function which updates the position of the
+  // doodler
+  updatePosition() {
+    this.prevY = this.y;
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.vy > 3.5) {
+      this.vy = 3.5;
+    } else {
+      this.vy += this.gravity;
     }
 
-    // function which updates the position of the 
-    // doodler
-    updatePosition() {
-        this.prevY = this.y;
-        this.x += this.vx;
-        this.y += this.vy;
-        if(this.vy > 3.5) {
-            this.vy = 3.5;
-        } else {
-            this.vy += this.gravity;
-        }
+    this.checkForWrapDoodler();
+    this.checkCollisionWithPlatforms();
+  }
 
-        this.checkForWrapDoodler();
-        this.checkCollisionWithPlatforms();
+  // function which checks whether
+  // our doodler is outside the canvas horizontally
+  // f.e. doodler leaves the canvas to the left
+  // he will enter back into it on the right
+  // and vice versa
+  checkForWrapDoodler() {
+    if (this.x + this.width < 0) {
+      this.x = canvas.width;
+    } else if (this.x > canvas.width) {
+      this.x = 0 - this.width;
+    }
+  }
+
+  // now we need to add a hitcollision so our doodler can jump upwards
+  checkCollisionWithPlatforms() {
+    // this here ensures that we only check if there is a collision
+    // while our doodler is falling down
+    if (this.vy <= 0) {
+      return;
     }
 
-    // function which checks whether
-    // our doodler is outside the canvas horizontally
-    // f.e. doodler leaves the canvas to the left
-    // he will enter back into it on the right
-    // and vice versa
-    checkForWrapDoodler() {
-        if(this.x + this.width < 0) {
-            this.x = canvas.width;
-        } else if(this.x > canvas.width) {
-            this.x = 0 - this.width;
-        }
+    // we make sure that the doodler is coming from above the
+    // platform and the other conditions is a check whether
+    // two boxes intersect
+    for (let i = 0; i < platForms.length; i++) {
+      let platform = platForms[i];
+      if (
+        this.prevY + this.height + 20 >= platform.y &&
+        this.x + this.width > platform.x &&
+        this.x < platform.x + platform.width &&
+        this.y + this.height > platform.y &&
+        this.y < platform.y + platform.height &&
+        this.prevY < platform.y
+      ) {
+        this.jump(platform);
+      }
     }
+  }
 
-    // now we need to add a hitcollision so our doodler can jump upwards
-    checkCollisionWithPlatforms() {
-        // this here ensures that we only check if there is a collision
-        // while our doodler is falling down
-        if(this.vy <= 0) {
-            return;
-        }
-
-        // we make sure that the doodler is coming from above the
-        // platform and the other conditions is a check whether
-        // two boxes intersect
-        for(let i = 0; i < platForms.length; i++) {
-            let platform = platForms[i];
-            if(
-                (this.prevY + this.height + 20) >= platform.y &&
-                this.x + this.width > platform.x &&
-                this.x < platform.x + platform.width &&
-                this.y + this.height > platform.y &&
-                this.y < platform.y + platform.height &&
-                this.prevY < platform.y
-            ) {
-                this.jump(platform);
-            }
-        }
+  // this if in there ensures that our doodler does not jump too fast
+  // upwards, because we would lose him then
+  jump(platform) {
+    let newHeight = platform.y - this.height;
+    if (newHeight > canvas.height / 2 - 120) {
+      this.y = platform.y - this.height;
+      this.vy = this.jumpStrength;
     }
+  }
 
-    // this if in there ensures that our doodler does not jump too fast
-    // upwards, because we would lose him then
-    jump(platform) {
-        let newHeight = platform.y - this.height;
-        if(newHeight > (canvas.height / 2 - 120)) {
-            this.y = platform.y - this.height;
-            this.vy = this.jumpStrength;
-        }
-    }
+  moveRight() {
+    this.vx += 4;
+    this.image.src = "./assets/personnage.png";
+  }
 
-    moveRight() {
-        this.vx += 4;
-        this.image.src = './assets/personnage.png';
-    }
+  moveLeft() {
+    this.vx -= 4;
+    this.image.src = "./assets/personnage.png";
+  }
 
-    moveLeft () {
-        this.vx -= 4;
-        this.image.src = './assets/personnage.png';
-    }
-
-    draw() {
-        this.context.drawImage(this.image, this.x, this.y, this.width, this.height);
-    }
+  draw() {
+    this.context.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
 }
 
 class Platform {
-    // constructor for the class platform
-    // x for the horizontal position and
-    // y for the vertical position
-    constructor(x, y) {
-        this.context = canvas.getContext("2d");
-        this.image = new Image();
-        this.image.src = 'assets/platform.png';
-        this.x = x;
-        this.y = y;
-        this.width = 100;
-        this.height = 20;
-    }
+  // constructor for the class platform
+  // x for the horizontal position and
+  // y for the vertical position
+  constructor(x, y) {
+    this.context = canvas.getContext("2d");
+    this.image = new Image();
+    this.image.src = "/assets/plateform.png";
+    this.x = x;
+    this.y = y;
+    this.width = 80;
+    this.height = 20;
+  }
 
-    // this updateposition works for the platforms
-    // we need to make them move downwards, so it seems like
-    // the doodler is going up!
+  // this updateposition works for the platforms
+  // we need to make them move downwards, so it seems like
+  // the doodler is going up!
 
-    draw() {
-        this.context.drawImage(this.image, this.x, this.y, this.width, this.height);
-    }
-
+  draw() {
+    this.context.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
 }
 
 let platForms = [];
 const doodler = new Doodler();
 
-// helper function to get us a random
-// number in between two numbers
-// we need to position the platforms later on
+// Aide a faire apparaitre les plateformes de maniere aleatoire
 function randomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-} 
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-// to display the end menu we saw previously
+// Pokeball qui tombe a la fin du jeu
+var pokeball = new Image();
+pokeball.src = "/assets/pokeball.png";
+
+// Création de la classe Ball
+class Ball {
+  constructor(x, y, width, height, dy) {
+    this.x = x;
+    this.y = y;
+    this.width = 350;
+    this.height = 100;
+    this.dy = dy;
+  }
+
+  // Méthode pour dessiner l'image
+  draw() {
+    ctx.drawImage(pokeball, this.x, this.y, this.width, this.height);
+  }
+
+  // Méthode pour faire tomber l'image
+  fall() {
+    this.y += this.dy;
+  }
+}
+
+// Création de plusieurs images aléatoires
+var balls = [];
+for (var i = 0; i < 10; i++) {
+  var x = Math.random() * canvas.width;
+  var y = Math.random() * -canvas.height;
+  var width = Math.random() * 100 + 50;
+  var height = Math.random() * 100 + 50;
+  var dy = Math.random() * 5 + 1;
+  var ball = new Ball(x, y, width, height, dy);
+  balls.push(ball);
+}
+
+// Fonction pour dessiner toutes les images et les faire tomber
+function drawBalls() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].fall();
+  }
+  requestAnimationFrame(drawBalls);
+}
+
+var magicarpe = new Image();
+magicarpe.src = "/assets/magicarp.png";
+
+// Position de départ de l'image
+var x = canvas.width / 2 - magicarpe.width / 2;
+var y = canvas.height - magicarpe.height;
+
+// Vitesse de déplacement de l'image
+var dx = 2;
+var dy = 0;
+
+// Amplitude et période de la fonction sinusoïdale
+var amplitude = 50;
+var period = 100;
+
+// Fonction pour dessiner l'image et la faire bouger en cloche
+function jumpMagicarpe() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.jumpMagicarp(magicarpe, x, y);
+  x += dx;
+  y = canvas.height - magicarpe.height / 2 + amplitude * Math.sin(x / period);
+  requestAnimationFrame(jumpMagicarpe);
+}
+
+// Personnage qui bouge avant le jeu
+var img = new Image();
+img.src = "/assets/personnage.png";
+
+img.onload = function () {
+// Position de départ de l'image
+var x = canvas.width / 2 - img.width / 2;
+var y = canvas.height - img.height;
+
+// Vitesse de déplacement de l'image
+var dx = 2;
+
+// Fonction pour dessiner l'image et la faire bouger
+function drawImage() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(img, x, y);
+  x += dx;
+  if (x > canvas.width - img.width || x < 0) {
+    dx = -dx;
+  }
+  requestAnimationFrame(drawImage);
+}
+drawImage();
+}
+//******************************* */
+
+// Menu de fin de partie
 function showEndMenu() {
-    document.getElementById('end-game-menu').style.display = 'block';
-    document.getElementById('end-game-score').innerHTML = score;
+  document.getElementById("end-game-menu").style.display = "block";
+  document.getElementById("end-game-score").innerHTML = score;
+  drawBalls();
 
-    // well we need to add the score...
-    if(highScore < score) {
-        highScore = score;
-    }
+  if (highScore < score) {
+    highScore = score;
+  }
 
-    document.getElementById('high-score').innerHTML = highScore;
+  document.getElementById("high-score").innerHTML = highScore;
 }
 
 function hideEndMenu() {
-    document.getElementById('end-game-menu').style.display = 'none';
+  document.getElementById("end-game-menu").style.display = "none";
 }
 
+// Pour commencer le jeu
+function startGame() {
+  document.querySelector(".start").style.display = "none";
+  // On lance la boucle de jeu
+  loop();
+  jumpMagicarpe();
+}
 
-// this is a function which adds us all the listeners
-// we need to be able to play doodle jump
+//Ajout des events aux touches
 function addListeners() {
-    document.addEventListener('keydown', function(event) {
-        if(event.code === "ArrowLeft") {
-            doodler.moveLeft();
-        } else {
-            doodler.moveRight();
-        }
-    });
-
-    document.addEventListener('keyup', function(event) {
-        if(event.code === "ArrowLeft" || event.code === "ArrowRight") {
-            doodler.vx = 0;
-        }
-    });
-
-    document.getElementById("retry").addEventListener('click', function() {
-        hideEndMenu();
-        resetGame();
-        loop();
-    });
-}
-
-// this is a function which will create us the platforms for
-// the beginning
-// the platformgap here will be needed so the platforms dont stack
-// on top of each other and to position them vertically
-function createPlatforms(platFormCount) {
-    platFormGap = Math.round(canvas.height / platFormCount);
-
-    for(let i = 0; i < platFormCount; i++) {
-        // now we will add here a do while loop
-        // it is needed so we create the platforms always to the left
-        // and the right and not in the middle, which is important
-        // in the beginning of the game
-        let xpos = 0;
-        do {
-            xpos = randomInteger(25, canvas.width - 25 - 100);
-        } while (
-            xpos > canvas.width / 2 - 100 * 1.5 &&
-            xpos < canvas.width / 2 + 100 / 2
-        )
-        let y = (canvas.height / 1.5) - i * platFormGap;
-        platForms.push(new Platform(xpos, y));   
+  document.addEventListener("keydown", function (event) {
+    if (event.code === "ArrowLeft") {
+      doodler.moveLeft();
+    } else {
+      doodler.moveRight();
     }
+  });
+
+  document.addEventListener("keyup", function (event) {
+    if (event.code === "ArrowLeft" || event.code === "ArrowRight") {
+      doodler.vx = 0;
+    }
+  });
+  document.querySelector(".start").addEventListener("click", function () {
+    startGame();
+  });
+
+  document.getElementById("retry").addEventListener("click", function () {
+    hideEndMenu();
+    resetGame();
+    loop();
+  });
 }
 
-// we push as first platform a platform exactly there where the
-// the doodler is so we can then start the game when we move 
-// left or right
+//Creation des plateformes
+function createPlatforms(platFormCount) {
+  platFormGap = Math.round(canvas.height / platFormCount);
+
+  for (let i = 0; i < platFormCount; i++) {
+    let xpos = 0;
+    do {
+      xpos = randomInteger(25, canvas.width - 25 - 150);
+    } while (
+      xpos > canvas.width / 2 - 100 * 1.5 &&
+      xpos < canvas.width / 2 + 100 / 2
+    );
+    let y = canvas.height / 1.5 - i * platFormGap;
+    platForms.push(new Platform(xpos, y));
+  }
+}
+
+//Affichage debut de la partie
 function setup() {
-    platForms.push(new Platform(doodler.x, (doodler.y + 80)));
-    createPlatforms(6);
+  platForms.push(new Platform(doodler.x, doodler.y + 50));
+  createPlatforms(6);
 }
 
+//Reset du jeu
 function resetGame() {
-    // now we need to be able to reset the game and also end the game
-    // if the doodler falls down
-    doodler.x = canvas.width / 2;
-    doodler.y = canvas.height - 100;
-    doodler.vx = 0;
-    doodler.vy = 0;
-    score = 0;
-    // we need to reset the platforms as well
-    platForms = [];
-    setup();
+  doodler.x = canvas.width / 2;
+  doodler.y = canvas.height - 100;
+  doodler.vx = 0;
+  doodler.vy = 0;
+  score = 0;
+  platForms = [];
+  setup();
 }
 
-// to display the score in the top middle of the canvas
+//Score lors du jeu
 function scoreText() {
-    doodler.context.font = '20px Arial';
-    doodler.context.fillStyle = 'black';
-    doodler.context.textAlign = 'center';
-    doodler.context.fillText(`Score: ${Math.round(score)}`, canvas.width / 2, 50);
+  doodler.context.font = "20px Arial";
+  doodler.context.fillStyle = "black";
+  doodler.context.textAlign = "center";
+  doodler.context.fillText(`Score: ${Math.round(score)}`, canvas.width / 2, 50);
 }
-
 
 // function to update the platforms - meaning we remove the ones which
 // are not visible anymore and simultaneously we update the score
 function updatePlatformsAndScore() {
-    // this creates a copy of our array of platforms
-    let platformsCpy = [...platForms];
-    platForms = platForms.filter(platform_ => platform_.y < canvas.height);
-    score += platformsCpy.length - platForms.length;
+  // this creates a copy of our array of platforms
+  let platformsCpy = [...platForms];
+  platForms = platForms.filter((platform_) => platform_.y < canvas.height);
+  score += platformsCpy.length - platForms.length;
 }
 
 function loop() {
-    doodler.context.clearRect(0, 0, canvas.width, canvas.height);
+  doodler.context.clearRect(0, 0, canvas.width, canvas.height);
 
-    if(doodler.y < canvas.height / 2 && doodler.vy < 0) {
-        // we need to have it here and not in the platform class
-        // now we additionally need to create more platforms as we move up
-        platForms.forEach(platform => {
-            platform.y += -doodler.vy * 2;
-        });
-
-        // we create here infinitely more platforms if we move up
-        platForms.push(new Platform(randomInteger(25, canvas.width - 25 - 100),
-            platForms[platForms.length - 1].y - platFormGap * 2));
-    }
-
-    doodler.draw();
-    doodler.updatePosition();
-
-    platForms.forEach(platform => {
-        platform.draw();
+  if (doodler.y < canvas.height / 2 && doodler.vy < 0) {
+    // we need to have it here and not in the platform class
+    // now we additionally need to create more platforms as we move up
+    platForms.forEach((platform) => {
+      platform.y += -doodler.vy * 3;
     });
 
-    scoreText();
-    // here we check whether the doodler fell down below all platforms
-    if(doodler.y > canvas.height) {
-        showEndMenu();
-        return;
-    }
+    // we create here infinitely more platforms if we move up
+    platForms.push(
+      new Platform(
+        randomInteger(25, canvas.width - 25 - 100),
+        platForms[platForms.length - 2].y - platFormGap * 2
+      )
+    );
+  }
 
-    updatePlatformsAndScore()
+  doodler.draw();
+  doodler.updatePosition();
 
-    requestAnimationFrame(loop);
+  platForms.forEach((platform) => {
+    platform.draw();
+  });
+
+  scoreText();
+  // here we check whether the doodler fell down below all platforms
+  if (doodler.y > canvas.height) {
+    showEndMenu();
+    return;
+  }
+
+  updatePlatformsAndScore();
+
+  requestAnimationFrame(loop);
 }
 
 addListeners();
 setup();
-loop();
+
